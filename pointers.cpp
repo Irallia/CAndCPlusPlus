a36cb5264ba39fa36f6f1224c9fc2733c007f246
MIRACULIX\Irallia <irallia@chrigelyra.de>
2019-09-26 14:42:28 +0200

    Tutorial: more about pointers

--- pointers.cpp
+++ pointers.cpp
@@ -5,8 +5,49 @@ using namespace std;
 
 #define LOG(x) cout << x << endl
 
-int main()
-{
+// Pointers are memory addresses
+/*
+* Pointers are simply a variable that hold an address so one could argue that a pointer is a data type, but it is not
+* defined as a data type (per "The C Programming Language". Kernighan & Ritchie).
+*/
+
+int main() {
+    /*
+    * Variables are stored in the Memory of the Computer (RAM).
+    * Each value is stored in a Container in the Memory, which has an address.
+    * When the Computer wants to access the Value, it needs the Memory address.
+    * In C++ we call these addresses pointers.
+    */
+    int age = 19;
+    double gpa =2.7;
+    string name = "Mike";
+
+    // We can print out this address/pointer, when we use the ampersand (&):
+
+    cout << "Age: " << &age << endl;   // -> something like: 0x61fea4
+    cout << "Gpa: " << &gpa << endl;
+    cout << "Name: " << &name << endl;
+
+    // If we want to save the pointer in a variable, we use the asterisk (*) and create it based of our variable:
+
+    int *pAge = &age;
+    double *pGpa = &gpa;
+    string *pName = &name;
+
+    // Without the asterisk (*), we get the pointer:
+
+    cout << "Our Addresses are: " << pAge << ", " << pGpa << " and "  << pName << endl;
+
+    // Dereference a pointer:
+
+    cout << "Inside of the Addresses we have: " << *pAge << ", " << *pGpa << " and "  << *pName << endl;
+    cout << "\e[1m*&age\e[0m ist the same as \e[1m*pAge\e[0m  and \e[1mage\e[0m: "
+        << *pAge << " = " << *&age << " = " << age << endl;
+    cout << "But \e[1m&*&age\e[0m ist the same as \e[1mpAge\e[0m and \e[1m&age\e[0m: "
+        << &*&age << " = " << pAge << " = " << &age << endl;
+
+    // --------------------------------------------------------------------------------------
+
     void* emptyPtr = nullptr;      // Empty pointer (same as void* ptr = 0 or NULL;)
 
     int var = 8;                // int is 4 bytes
@@ -30,7 +71,5 @@ int main()
     char** secPtr = &buffer;       // pointer to a pointer
 
     cout << "Our buffer=" << buffer << " is at the memory position: " << secPtr << endl;
-
-    cin.get();
 
 }
