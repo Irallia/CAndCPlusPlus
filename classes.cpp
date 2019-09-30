c517583798d17f666b39568f95973fb9f6678be3
MIRACULIX\Irallia <irallia@chrigelyra.de>
2019-09-30 15:26:26 +0200

    Tutorial: Classes, variables and functions, public and private, get-ers and set-ers

--- classes.cpp
+++ classes.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 // Represent data by creat my own datatype, with the help of a class. (-> map a blueprint for your datatype)
 class Book {
+    // only code thats inside the class Book can access whats in private:
+    private:
+        // if you want to print out the rating in the main function, it doesn't work
+        string rating;
     public:
         string title;
         string author;
@@ -12,13 +16,15 @@ class Book {
             title = "no title";
             author = "no author";
             pages = -1;
-            cout << "Creating Book Object: " << title;
+            rating = "NR";
+            cout << "Creating Book Object: " << title << endl;
         }
-        Book(string aTitle, string aAuthor, int aPages) {
-            cout << "Creating Book Object: " << aTitle;
+        Book(string aTitle, string aAuthor, int aPages, string aRating) {
+            cout << "Creating Book Object: " << aTitle << endl;
             title = aTitle;
             author = aAuthor;
             pages = aPages;
+            setRating(aRating);
             cout << " with " << pages << " Pages." << endl;
         }
         // Class functions:
@@ -29,18 +35,48 @@ class Book {
             }
             return false;
         }
+        // get-ers and set-ers:
+        // use set functions to set up some rules:
+        void setRating(string aRating) {
+            if (aRating == "G" || aRating == "PG" || aRating == "PG-13" || aRating == "R" || aRating == "NR") {
+                rating = aRating;
+            } else {
+                clog << "--- You set an invalid rating. ---" << endl;
+                rating = "NR";
+            }
+        }
+        // for private variables:
+        string getRating() {
+            return rating;
+        }
 };
 
 int main()
 {
     // Now we create a book: (create an instance of our class, its an object specified by our class)
-    Book book1("Harry Potter", "JK Rowling", 400);
+    Book book1("Harry Potter", "JK Rowling", 400, "PG-13");
     book1.pages = 500;
-
     cout << "Now it has " << book1.pages << " pages." << endl;
 
-    Book book2("My Book", "Ira", 7000);
+    // cout << book1.rating << endl;
+    // inaccessible, because rating is private:
+    // but:
+    book1.setRating("Dog");
+    cout << book1.getRating() << endl;
+
+    Book book2("My Book", "Ira", 7000, "bla");
     Book book3;
+
+    cout << "Which of our books is long?" << endl;
+    Book books[] = {book1, book2, book3};
+
+    for (int i = 0; i < 3; i++) {
+        if (books[i].isLongBook()) {
+            cout << books[i].title << " is " << books[i].pages << " long." << endl;
+        } else {
+            cout << books[i].title << " is " << books[i].pages << " short." << endl;
+        }
+    };
 
     return 0;
 }
